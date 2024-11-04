4362b312e0cd5ea2d430f419e9074a02db14c6f4
윤성현 <shyoon840@gmail.com>
2024-11-04 05:25:27 +0000

    implement SLL_InsertBefore

--- DS/List/LinkedList/LinkedList.h
+++ DS/List/LinkedList/LinkedList.h
@@ -16,6 +16,7 @@ void SLL_DestroyNode(Node* Node);
 void SLL_AppendNode(Node** Head, Node* NewNode);
 void SLL_InsertAfter(Node* Current, Node* NewNode);
 void SLL_InsertBefore(Node** Head, Node* Current, Node* NewHead);
+void SLL_InsertBeforeFixed(Node** Head, Node* Current, Node* NewHead);
 void SLL_InsertNewHead(Node** Head, Node* NewNode);
 void SLL_RemoveNode(Node** Head, Node* Remove);
 Node* SLL_GetNodeAt(Node* Head, int Location);
