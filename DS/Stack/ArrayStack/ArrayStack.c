a0244dea3dd63113b1546cdc126a3345ea52db43
윤성현 <shyoon840@gmail.com>
2024-11-17 10:38:25 +0000

    Solve Stack Pratice Problem

--- DS/Stack/ArrayStack/ArrayStack.c
+++ DS/Stack/ArrayStack/ArrayStack.c
@@ -1,47 +1,63 @@
 #include "ArrayStack.h"
 
 // 스택 및 노드 생성
-void AS_CreateStack(ArrayStack** Stack, int Capacity){
-	(*Stack) = (ArrayStack*)malloc(sizeof(Stack));
-	
-	(*Stack)->Nodes = (Node*)malloc(sizeof(Node)*Capacity);
-	(*Stack)->Capacity = Capacity;
-	(*Stack)->Top = -1;
+void AS_CreateStack(ArrayStack **Stack, int Capacity) {
+  (*Stack) = (ArrayStack *)malloc(sizeof(Stack));
+
+  (*Stack)->Nodes = (Node *)malloc(sizeof(Node) * Capacity);
+  (*Stack)->Capacity = Capacity;
+  (*Stack)->Top = -1;
 }
 
 // 스택 및 노드 소멸
-void AS_DestroyStack(ArrayStack* Stack){
-	free(Stack->Nodes);
-	free(Stack);
+void AS_DestroyStack(ArrayStack *Stack) {
+  free(Stack->Nodes);
+  free(Stack);
 }
 
 // 노드 삽입
-void AS_Push(ArrayStack* Stack, ElementType Data){
-	Stack->Top++;
-	Stack->Nodes[Stack->Top].Data = Data;
+void AS_Push(ArrayStack *Stack, ElementType Data) {
+  Stack->Top++;
+  Stack->Nodes[Stack->Top].Data = Data;
+}
+
+// 노드 삽입 개선 - 용량 모두 소진 시 현재 용량의 30%만큼 늘리기
+void AS_Push_Improved(ArrayStack *Stack, ElementType Data) {
+
+  if (AS_IsFull(Stack)) {
+    Stack->Capacity = Stack->Capacity * 1.3;
+    Stack->Nodes =
+        (Node *)realloc(Stack->Nodes, sizeof(Node) * (Stack->Capacity));
+  }
+  Stack->Top++;
+  Stack->Nodes[Stack->Top].Data = Data;
 }
 
 // 노드 제거
-ElementType AS_Pop(ArrayStack* Stack){
-	
-	int Position = Stack->Top--;
-	
-	return Stack->Nodes[Position].Data;
+ElementType AS_Pop(ArrayStack *Stack) {
+
+  int Position = Stack->Top--;
+
+  return Stack->Nodes[Position].Data;
 }
 
-ElementType AS_Top(ArrayStack* Stack){	
-	return Stack->Nodes[Stack->Top].Data;
+// 노드 제거 - 용량이 70% 미만으로 내려갈 시 현재 용량의 30%만큼 줄이기
+ElementType AS_Pop_Improved(ArrayStack *Stack) {
+
+  int Position = Stack->Top--;
+
+  if((float)(Stack->Top+1)/Stack->Capacity < 0.7){
+	Stack->Capacity = Stack->Capacity * 0.7;
+	Stack->Nodes = (Node*)realloc(Stack->Nodes, sizeof(Node*) * (Stack->Capacity));
+  }
+
+  return Stack->Nodes[Position].Data;
 }
 
-int AS_GetSize(ArrayStack *Stack){
-	return Stack->Top+1;
-}
+ElementType AS_Top(ArrayStack *Stack) { return Stack->Nodes[Stack->Top].Data; }
 
-int AS_IsEmpty(ArrayStack *Stack){
-	return (Stack->Top == -1);
-}
+int AS_GetSize(ArrayStack *Stack) { return Stack->Top + 1; }
 
-int AS_IsFull(ArrayStack* Stack){
-	return (Stack->Top+1 == Stack->Capacity);
-}
+int AS_IsEmpty(ArrayStack *Stack) { return (Stack->Top == -1); }
 
+int AS_IsFull(ArrayStack *Stack) { return (Stack->Top + 1 == Stack->Capacity); }
--- DS/Stack/ArrayStack/Test_ArrayStack.c
+++ DS/Stack/ArrayStack/Test_ArrayStack.c
@@ -4,12 +4,18 @@ int main(void) {
   int i = 0;
   ArrayStack *Stack = NULL;
 
-  AS_CreateStack(&Stack, 4);
+  AS_CreateStack(&Stack, 10);
 
-  AS_Push(Stack, 3);
+  AS_Push(Stack, 1);
+  AS_Push(Stack, 11111);
   AS_Push(Stack, 37);
-  AS_Push(Stack, 11);
-  AS_Push(Stack, 12);
+  AS_Push(Stack, 1123);
+  AS_Push(Stack, 342);
+  AS_Push(Stack, 122);
+  AS_Push(Stack, 542);
+  AS_Push(Stack, 642);
+  AS_Push(Stack, 152);
+  AS_Push(Stack, 1342);
 
   printf("Capacity: %d, Size: %d, Top: %d\n\n", Stack->Capacity,
          AS_GetSize(Stack), AS_Top(Stack));
@@ -18,13 +24,16 @@ int main(void) {
     if (AS_IsEmpty(Stack))
       break;
 
-    printf("Popped: %d, ", AS_Pop(Stack));
+    printf("Popped: %d, ", AS_Pop_Improved(Stack));
 
     if (!AS_IsEmpty(Stack))
       printf("Current Top: %d\n", AS_Top(Stack));
     else
       printf("Stack Is Empty.\n");
   }
+
+  printf("Capacity: %d, Size: %d, Top: %d\n\n", Stack->Capacity,
+         AS_GetSize(Stack), AS_Top(Stack));
 
   if (AS_IsFull(Stack)) {
     printf("Is Full\n");
