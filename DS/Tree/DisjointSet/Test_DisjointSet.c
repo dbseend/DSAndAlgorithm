7f7326f07c47e7863b1fc267aa5b8a8f60207edb
윤성현 <shyoon840@gmail.com>
2024-11-30 05:38:01 +0000

    implement disjoint

--- DS/Tree/DisjointSet/Test_DisjointSet.c
+++ DS/Tree/DisjointSet/Test_DisjointSet.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
 #include "DisjointSet.h"
 
-int main( void )
-{
-	int a=1, b=2, c=3, d=4;
-	DisjointSet* Set1 = DS_MakeSet(&a);
-	DisjointSet* Set2 = DS_MakeSet(&b);
-	DisjointSet* Set3 = DS_MakeSet(&c);
-	DisjointSet* Set4 = DS_MakeSet(&d);
+int main(void) {
+  char a = 'a', b = 'n', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g';
+  DisjointSet *set1 = DS_MakeSet(&a);
+  DisjointSet *set2 = DS_MakeSet(&b);
+  DisjointSet *set3 = DS_MakeSet(&c);
+  DisjointSet *set4 = DS_MakeSet(&d);
+  DisjointSet *set5 = DS_MakeSet(&e);
+  DisjointSet *set6 = DS_MakeSet(&f);
+  DisjointSet *set7 = DS_MakeSet(&g);
 
-    printf("Set1 == Set2 : %d \n", DS_FindSet( Set1 ) == DS_FindSet( Set2 ) );
+  DS_UnionSet(set1, set2);
+  DS_UnionSet(set1, set3);
 
-	DS_UnionSet( Set1, Set3 );
-    printf("Set1 == Set3 : %d \n", DS_FindSet( Set1 ) == DS_FindSet( Set3 ) );
+  DS_UnionSet(set4, set5);
+  DS_UnionSet(set4, set6);
+  DS_UnionSet(set4, set7);
 
-    DS_UnionSet( Set3, Set4 );
-    printf("Set3 == Set4 : %d \n", DS_FindSet( Set3 ) == DS_FindSet( Set4 ) );
+  DS_UnionSet(set1,set4);
 
-	return 0;
+  return 0;
 }
