1f7016ce289247dedf4913667c6d972d58e50fd7
윤성현 <shyoon840@gmail.com>
2025-04-07 13:04:31 +0000

    implment SHH

---
+++ Algorithm/Hash/Chaning/Chaining.c
@@ -0,0 +1,42 @@
+#include "Chaining.h"
+
+HashTable* CHT_CreateHashTable( int TableSize )
+{
+
+}
+
+Node* CHT_CreateNode( KeyType Key, ValueType Value )
+{
+
+}
+
+void CHT_DestroyNode( Node* TheNode )
+{
+
+}
+
+void CHT_Set( HashTable* HT, KeyType Key, ValueType Value )
+{
+
+}
+
+ValueType CHT_Get( HashTable* HT, KeyType Key )
+{
+
+}
+
+void CHT_DestroyList( List L )
+{
+
+}
+
+void CHT_DestroyHashTable( HashTable* HT)
+{
+
+}
+
+int CHT_Hash( KeyType Key, int KeyLength, int TableSize )
+{
+
+}
+                              
